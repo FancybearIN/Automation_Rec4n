6154ba35771a4cb317bcaa4d74bef2ddd1d734ca
FancybearIN <itdeep0007@gmail.com>
2025-05-01 12:54:51 +0530

    result

--- recom.sh
+++ recom.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 
-echo "@fancybearin"
-echo "Ankit thakur"
+# Display a creative banner
+echo "==============================================="
+echo " ███╗   ██╗ ██████╗     █████╗ ████████╗██████╗ "
+echo " ████╗  ██║██╔═══██╗   ██╔══██╗╚══██╔══╝██╔══██╗"
+echo " ██╔██╗ ██║██║   ██║   ███████║   ██║   ██████╔╝"
+echo " ██║╚██╗██║██║   ██║   ██╔══██║   ██║   ██╔═══╝ "
+echo " ██║ ╚████║╚██████╔╝██╗██║  ██║   ██║   ██║     "
+echo " ╚═╝  ╚═══╝ ╚═════╝ ╚═╝╚═╝  ╚═╝   ╚═╝   ╚═╝     "
+echo "==============================================="
+echo "       Next-Gen Advanced Recon Framework       "
+echo "==============================================="
+echo "                   NG-APTF                     "
+echo "==============================================="
 
 # Ensure the user provides a domain
 if [ -z "$1" ]; then
@@ -20,17 +31,16 @@ create_output_dirs() {
 # Subdomain enumeration
 enumerate_subdomains() {
     echo "[*] Enumerating subdomains for $domain..."
-    subfinder -d $domain | tee -a recon_results/$domain/subdomains/sub1.txt 
-    assetfinder --subs-only $domain | tee -a recon_results/$domain/subdomains/sub2.txt 
-    echo "$domain" | chaos -key 8efc7ffa-0e70-4bf5-96a8-b60e73e80952 | tee -a recon_results/$domain/subdomains/sub4.txt 
-    echo "$domain" | alterx | tee -a recon_results/$domain/subdomains/sub5.txt 
+    subfinder -d $domain | tee -a recon_results/$domain/subdomains/sub1.txt
+    assetfinder --subs-only $domain | tee -a recon_results/$domain/subdomains/sub2.txt
+    echo "$domain" | chaos -key 8efc7ffa-0e70-4bf5-96a8-b60e73e80952 | tee -a recon_results/$domain/subdomains/sub4.txt
+    echo "$domain" | alterx | tee -a recon_results/$domain/subdomains/sub5.txt
     wait
-
     cat recon_results/"$domain"/subdomains/*.txt | sort -u | tee -a recon_results/"$domain"/subdomains/all_subdomains.txt
 }
 
 # Check for subdomain takeovers
-check_takeover() {
+#check_takeover() {
     echo "[*] Checking for subdomain takeovers..."
 
     # Filter out invalid entries
@@ -39,23 +49,24 @@ check_takeover() {
     # Run subjack with the cleaned file
     subjack -w recon_results/$domain/subdomains/all_subdomains_cleaned.txt -t 100 -timeout 30 -ssl \
         -c /home/kali/go/pkg/mod/github.com/haccer/subjack@v0.0.0-20201112041112-49c51e57deab/subjack/fingerprint.go -v | tee -a recon_results/$domain/result/takeover.txt
-}
+#}
 
 # Extract live URLs
 extract_links() {
     echo "[*] Extracting URLs..."
-    cat recon_results/$domain/subdomains/all_subdomains.txt | httpx -ports 80,443,8080,8443 | tee recon_results/$domain/extracted_urls/live_links.txt
+    cat recon_results/$domain/subdomains/all_subdomains.txt | httpx -ports 80,443,8080,8443 | tee -a recon_results/$domain/extracted_urls/live_links.txt
     cat recon_results/$domain/extracted_urls/live_links.txt | sort -u | tee -a recon_results/$domain/extracted_urls/unique_links.txt
     cat recon_results/$domain/extracted_urls/unique_links.txt | gau | tee -a recon_results/$domain/extracted_urls/extracted.txt
     cat recon_results/$domain/extracted_urls/unique_links.txt | waybackurls | tee -a recon_results/$domain/extracted_urls/extracted.txt
     wait
 
     cat recon_results/$domain/extracted_urls/extracted.txt | sort -u | tee -a recon_results/$domain/extracted_urls/cleaned_urls.txt
-    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | httpx | tee -a recon_results/$domain/extracted_urls/final_urls.txt
-    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | httprobe -c 10 | tee -a recon_results/$domain/extracted_urls/final_urls.txt
-    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | katana -d 4 -o recon_results/$domain/extracted_urls/final_urls.txt
-    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | galer -o recon_results/$domain/extracted_urls/final_urls.txt
+    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | httpx | tee -a recon_results/$domain/extracted_urls/final1.txt
+    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | httprobe -c 10 | tee -a recon_results/$domain/extracted_urls/final2.txt
+    cat recon_results/$domain/extracted_urls/cleaned_urls.txt | katana -d 4 -o recon_results/$domain/extracted_urls/final3.txt
+   # cat recon_results/$domain/extracted_urls/cleaned_urls.txt | galer -o recon_results/$domain/extracted_urls/final4.txt
     wait
+    cat recon_results/$domain/extracted_urls/*.txt | sort -u | tee -a recon_results/$domain/extracted_urls/final_urls.txt
 }
 
 # Perform GF pattern matching
@@ -72,6 +83,7 @@ gf_analysis() {
 
     for pattern in debug_logic idor img-traversal interestingEXT interestingparams interestingsubs jsvar lfi rce redirect sqli ssrf ssti xss; do
         echo "[*] Running gf for $pattern..."
+        mkdir -p recon_results/$domain/gf_parameters/$pattern
         cat recon_results/$domain/extracted_urls/final_urls.txt | gf $pattern | tee -a recon_results/$domain/gf_parameters/$pattern/$pattern.txt
     done
     wait
@@ -79,9 +91,25 @@ gf_analysis() {
 
 # Perform port scanning
 perform_port_scan() {
-    echo "[*] Performing port scan on $domain..."
-    nmap $domain -p1-65535 --rate=1000 | tee recon_results/$domain/port_scans/ports.txt
-    wait
+    echo "[*] Performing port scan on all subdomains for $domain..."
+
+    # Check if the subdomains file exists
+    local subdomains_file="recon_results/$domain/subdomains/all_subdomains.txt"
+    if [ ! -f "$subdomains_file" ]; then
+        echo "Error: Subdomains file not found at $subdomains_file"
+        return
+    fi
+
+    # Create output directory for port scans
+    mkdir -p recon_results/$domain/port_scans
+
+    # Iterate through each subdomain and perform a port scan
+    while read -r subdomain; do
+        echo "[*] Scanning ports for $subdomain..."
+        nmap -p- -T4 "$subdomain" | tee -a recon_results/$domain/port_scans/${subdomain}_ports.txt
+    done < "$subdomains_file"
+
+    echo "[+] Port scanning completed. Results saved in recon_results/$domain/port_scans/"
 }
 
 # Perform directory brute-forcing
@@ -97,7 +125,7 @@ directory_bruteforce() {
 test_vulnerabilities() {
     echo "[*] Running vulnerability tests..."
 
-    cat recon_results/$domain/extracted_urls/final_urls.txt | Gxss | tee -a recon_results/$domain/xss/gxss.txt
+   # cat recon_results/$domain/extracted_urls/final_urls.txt | Gxss | tee -a recon_results/$domain/xss/gxss.txt
     cat recon_results/$domain/xss/gxss.txt | dalfox pipe | tee -a recon_results/$domain/result/dalfox.txt
     wait
 
@@ -109,18 +137,19 @@ test_vulnerabilities() {
         -patterns ~/.git/ssrf-tool/important/patterns.txt | tee -a recon_results/$domain/result/ssrf2.txt
     wait
 
-    bash sqli recon_results/$domain/gf_parameters/sqli/sqli.txt | tee -a recon_results/$domain/result/sqli_sqlmap_result.txt
-    cat recon_results/$domain/extracted_urls/final_urls.txt | nuclei -t ./nuclei-templates/ -o recon_results/$domain/result/result.txt
+    #bash sqli recon_results/$domain/gf_parameters/sqli/sqli.txt | tee -a recon_results/$domain/result/sqli_sqlmap_result.txt
+    cat recon_results/$domain/extracted_urls/final_urls.txt | nuclei -t ~/.local/nuclei-templates/ -o recon_results/$domain/result/result.txt
     wait
 }
 
 # Execute all functions in order
-create_output_dirs
-enumerate_subdomains
-check_takeover & directory_bruteforce
-extract_links
-gf_analysis
-perform_port_scan
+# create_output_dirs
+# enumerate_subdomains
+#check_takeover 
+#directory_bruteforce
+# extract_links
+# gf_analysis
+# perform_port_scan
 test_vulnerabilities
 
 echo "[*] Recon process completed for $domain!"
